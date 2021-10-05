b8028fb241f1597941b0371447bb3a0376600d1d
askolt <keni319@yandex.ru>
2021-10-05 17:44:01 +0300

    v0.1.0 added basic program structure

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EasyRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
